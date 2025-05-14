549abe4a112cb82f91fa8281e91594ab70e471c6
divyaareddyy1998 <divyaareddyy1998@gmail.com>
2025-05-14 11:02:10 +0530

    episode-05

---
+++ src/components/Body.js
@@ -0,0 +1,24 @@
+import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/mockData";
+import { useState } from "react";
+const Body = () => {
+ const [restaurantList,setRestaurantList] =  useState(resList);
+    return (
+      <div className="body">
+        <div className="filter">
+          <button className="filter-btn" onClick={() => {
+             const  filteredList = restaurantList.filter((res) => res.info.avgRating > 4.3  )
+
+             setRestaurantList(filteredList);
+          }}>Top Rated Restaurants</button>
+        </div>
+        <div className="res-container">
+          {restaurantList.map((restaurant) => <RestaurantCard key={restaurant.info.id} resData={restaurant}/>)}
+                 
+        
+        </div>
+      </div>
+    );
+  };
+
+  export default Body;
---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import { HEADER_URL } from "../utils/constants";
+
+const Header = () => {
+    return (
+      <div className="header">
+        <div className="logocontainer">
+          <img
+            className="logo"
+            src={HEADER_URL}
+          />
+        </div>
+        <div className="nav-items">
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+          </ul>
+        </div>
+      </div>
+    );
+  };
+  export default Header;
